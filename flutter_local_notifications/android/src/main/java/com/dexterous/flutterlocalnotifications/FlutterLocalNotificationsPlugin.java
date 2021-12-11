f64b402622079b06ee50f5fdb468319afa26c4b0
Boopesh Mahendran <boopesh@cybermindworks.com>
2021-12-11 17:16:16 +0530

    fix

--- flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java
+++ flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java
@@ -250,6 +250,7 @@ public class FlutterLocalNotificationsPlugin
 
     if (notificationDetails.customNotificationType != null) {
         builder.setCustomContentView(getCustomNotification(context, notificationDetails));
+        builder.setCustomBigContentView(getCustomNotification(context, notificationDetails));
     }
 
     setSmallIcon(context, notificationDetails, builder);
